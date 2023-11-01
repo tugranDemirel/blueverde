75b20641192e585ce4807de302e6c871ca295a27
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-11-02 01:33:15 +0300

    wip

---
+++ app/Models/Category.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'parent_id',
+        'slug',
+        'description',
+        'image',
+        'is_active',
+        'is_featured',
+        'order',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'is_featured' => 'boolean',
+    ];
+}
